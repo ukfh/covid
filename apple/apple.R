139e17fe647278c003747743eb7b84653df53613
Uli Harder <github@harder.me.uk>
2021-04-05 10:32:52 +0100

    Apple update

--- apple/apple.R
+++ apple/apple.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(tidyquant)
 source('common.R')
 
-data <- read.csv('apple/applemobilitytrends-2021-03-28.csv', header = T, stringsAsFactors = F)
+data <- read.csv('apple/applemobilitytrends-2021-04-03.csv', header = T, stringsAsFactors = F)
 
 # turn this into a sane data format
 data <- data %>% pivot_longer(-(geo_type:country))
