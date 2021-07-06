bd6c1c0b36bec5262e22cc1a40915f47c1d92d24
Uli Harder <github@harder.me.uk>
2021-07-06 16:03:36 +0000

    new crontab

--- crontab.sh
+++ crontab.sh
@@ -1,3 +1,3 @@
-30 16,17 * * * /home/uh/R/covid/gov.sh
-30 9 * * *  /home/uh/R/covid/owid.sh
+30 15,16,17 * * * /home/uh/R/covid/gov.sh
+30 8 * * *  /home/uh/R/covid/owid.sh
 30 11 * * *    /home/uh/R/covid/google.sh
