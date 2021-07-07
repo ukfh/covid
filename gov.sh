5e7017b94cca26efb9bb707b2e8fabb1869d7dd3
Uli Harder <github@harder.me.uk>
2021-07-07 21:07:56 +0100

    Change in the prediction

--- gov.sh
+++ gov.sh
@@ -4,7 +4,7 @@ DIR=/home/uh/R/covid
 DATE=`date`
 
 cd $DIR
-/usr/local/bin/Rscript gov/corona_v2.R 
+/usr/local/bin/Rscript gov/corona_v3.R 
 
 git add gov/uk_covid_l*
 git commit -m "plot update on $DATE"
