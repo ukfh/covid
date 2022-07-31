917ca36f1666d38264e81b4016fdbee23c5af6d0
Uli Harder <github@harder.me.uk>
2022-07-31 19:23:34 +0000

    Deactivate Google & gov

--- crontab.sh
+++ crontab.sh
@@ -1,9 +1,9 @@
 MAILTO="uliharder@gmail.com"
 MAILFROM="uliharder@gmail.com"
 # covid
-30 16,17,18 * * 1-5 /home/uh/R/covid/gov.sh
-30 21 * * *  /home/uh/R/covid/owid.sh
-30 22 * * *    /home/uh/R/covid/google.sh
+#30 16,17,18 * * 1-5 /home/uh/R/covid/gov.sh
+30 9 * * *  /home/uh/R/covid/owid.sh
+#30 11 * * *    /home/uh/R/covid/google.sh
 
 # weather data
 15 1,5,9,13,17,21 * * * /home/uh/weather/weather/pipeline.sh
--- google/google.R
+++ google/google.R
@@ -4,13 +4,13 @@ library(RCurl)
 options(bitmapType='cairo')
 source('common.R')
 
-google.org <- getURL('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv', timeout = 2000)
+#google.org <- getURL('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv', timeout = 2000)
 #read.csv(, header = T, stringsAsFactors = F)
 
 #google.org <- read.csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv', header = T, stringsAsFactors = F)
-#google.org <- read.csv('google/2021-02-22_Global_Mobility_Report.csv',header = T, stringsAsFactors = F)
+google.org <- read.csv('google/Global_Mobility_Report.csv',header = T, stringsAsFactors = F)
 # save data ----
-write.csv(google.org, file = paste('google/', Sys.Date(), '_Global_Mobility_Report.csv', sep = ''))
+#write.csv(google.org, file = paste('google/', Sys.Date(), '_Global_Mobility_Report.csv', sep = ''))
 #google.org <- read.csv('google/2021-02-22_Global_Mobility_Report.csv', header = T, stringsAsFactors = F)
 google.org <- google.org %>% mutate(date = as.Date(date)) # , dow = weekdays(date)
 # google.org <- google.org %>% select(-dow)
